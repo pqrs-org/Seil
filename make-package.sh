f16e38db0e121cb6c591d03649057772733315ff
Takayama Fumihiko <tekezo@pqrs.org>
2008-05-06 22:22:00 +0900

    update Makefile

--- make-package.sh
+++ make-package.sh
@@ -5,8 +5,8 @@ version=$(cat version)
 packagemaker=/Developer/Applications/Utilities/PackageMaker.app/Contents/MacOS/PackageMaker
 pkgName="PCKeyboardHack-${version}.pkg"
 
-echo "char * const config_version = \"$version-Tiger\";" > src/kext/Tiger/version.hpp
-echo "char * const config_version = \"$version-Leopard\";" > src/kext/Leopard/version.hpp
+echo "char * const config_version = \"$version-Tiger\";" > src/core/kext/Tiger/version.hpp
+echo "char * const config_version = \"$version-Leopard\";" > src/core/kext/Leopard/version.hpp
 
 make clean build || exit $?
 
@@ -20,31 +20,34 @@ sudo mkdir -p pkgroot
 basedir="/Library/org.pqrs/PCKeyboardHack"
 sudo mkdir -p "pkgroot/$basedir"
 for ostype in Tiger Leopard; do
-    sudo cp -R src/kext/${ostype}/build/Release/PCKeyboardHack.kext "pkgroot/$basedir/PCKeyboardHack.${ostype}.kext"
+    sudo cp -R src/core/kext/${ostype}/build/Release/PCKeyboardHack.kext "pkgroot/$basedir/PCKeyboardHack.${ostype}.kext"
 done
-sudo cp -R files/extra "pkgroot/$basedir"
 sudo cp -R files/prefpane "pkgroot/$basedir"
 sudo cp -R files/scripts "pkgroot/$basedir"
 sudo cp -R files/share "pkgroot/$basedir"
 
+sudo mkdir -p "pkgroot/$basedir/extra"
+sudo cp -R pkginfo/Resources/preflight "pkgroot/$basedir/extra/uninstall.sh"
+
 sudo mkdir -p "pkgroot/Library"
 sudo cp -R files/LaunchDaemons pkgroot/Library
-sudo cp -R files/LaunchAgents pkgroot/Library
 
 sudo mkdir -p "pkgroot/$basedir/bin"
-sudo cp src/sysctl_confd/build/Release/PCKeyboardHack_sysctl_confd "pkgroot/$basedir/bin"
-sudo cp src/sysctl_ctl/build/Release/PCKeyboardHack_sysctl_ctl "pkgroot/$basedir/bin"
-sudo cp src/sysctl_reset/build/Release/PCKeyboardHack_sysctl_reset "pkgroot/$basedir/bin"
-sudo cp src/sysctl_set/build/Release/PCKeyboardHack_sysctl_set "pkgroot/$basedir/bin"
+sudo cp src/bin/set_loginwindow/build/Release/set_loginwindow "pkgroot/$basedir/bin"
+sudo cp src/bin/sysctl_confd/build/Release/PCKeyboardHack_sysctl_confd "pkgroot/$basedir/bin"
+sudo cp src/bin/sysctl_ctl/build/Release/PCKeyboardHack_sysctl_ctl "pkgroot/$basedir/bin"
+sudo cp src/bin/sysctl_reset/build/Release/PCKeyboardHack_sysctl_reset "pkgroot/$basedir/bin"
+sudo cp src/bin/sysctl_set/build/Release/PCKeyboardHack_sysctl_set "pkgroot/$basedir/bin"
 
 sudo mkdir -p "pkgroot/Library/PreferencePanes"
-sudo cp -R "src/prefpane/build/Release/PCKeyboardHack.prefPane" "pkgroot/Library/PreferencePanes"
+sudo cp -R "src/util/prefpane/build/Release/PCKeyboardHack.prefPane" "pkgroot/Library/PreferencePanes"
 
 sudo find pkgroot -type d -print0 | xargs -0 sudo chmod 755
 sudo find pkgroot -type f -print0 | xargs -0 sudo chmod 644
 sudo find pkgroot -name '*.sh' -print0 | xargs -0 sudo chmod 755
 sudo chmod 4755 pkgroot/$basedir/bin/PCKeyboardHack_sysctl_reset
 sudo chmod 4755 pkgroot/$basedir/bin/PCKeyboardHack_sysctl_set
+sudo chmod 755 pkgroot/$basedir/bin/set_loginwindow
 sudo chmod 755 pkgroot/$basedir/bin/PCKeyboardHack_sysctl_confd
 sudo chmod 755 pkgroot/$basedir/bin/PCKeyboardHack_sysctl_ctl
 sudo chown -R root:wheel pkgroot
