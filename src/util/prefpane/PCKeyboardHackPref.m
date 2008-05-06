3f5126bd140860ab44f7736d1392a722c4b2fd1f
tekezo <devnull@localhost>
2008-05-06 22:46:16 +0900

    update prefpane

--- src/util/prefpane/PCKeyboardHackPref.m
+++ src/util/prefpane/PCKeyboardHackPref.m
@@ -14,9 +14,25 @@
   [_versionText setStringValue:version];
 }
 
+// ----------------------------------------------------------------------
+- (void) registerLoginWindow
+{
+  NSString *app = @"/Library/org.pqrs/PCKeyboardHack/app/PCKeyboardHack_launchd.app";
+
+  NSString *set_loginwindow = @"/Library/org.pqrs/PCKeyboardHack/bin/set_loginwindow";
+  NSString *result = [BUNDLEPREFIX_Common getExecResult:set_loginwindow args:[NSArray arrayWithObjects:@"exist", app, nil]];
+  if ([result intValue] == 0) {
+    [[NSWorkspace sharedWorkspace] launchApplication:app];
+
+    [BUNDLEPREFIX_Common getExecResult:set_loginwindow args:[NSArray arrayWithObjects:@"set", app, nil]];
+  }
+}
+
+// ----------------------------------------------------------------------
 - (void) mainViewDidLoad
 {
   [self drawVersion];
+  [self registerLoginWindow];
 }
 
 @end
