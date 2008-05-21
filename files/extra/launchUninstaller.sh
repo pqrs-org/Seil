0c8ec284d3607d782545349e7fa1047eb0b55e52
Takayama Fumihiko <tekezo@pqrs.org>
2008-05-21 10:26:41 +0900

    update around uninstaller

--- files/extra/launchUninstaller.sh
+++ files/extra/launchUninstaller.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-TMPDIR=`/usr/bin/mktemp -d /tmp/uninstaller.XXXXXX` || exit 1
+TMPDIR=`/usr/bin/mktemp -d /tmp/PCKeyboardHack_uninstaller.XXXXXX` || exit 1
 /usr/bin/rsync -a /Library/org.pqrs/PCKeyboardHack/app/uninstaller.app "$TMPDIR"
 /usr/bin/open "$TMPDIR/uninstaller.app"
