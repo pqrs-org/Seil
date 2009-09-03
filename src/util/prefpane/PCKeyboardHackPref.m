acd18b3e439ad5c451e2f6b6a0cbc8a3cbadcb35
tekezo <devnull@localhost>
2009-09-03 23:28:27 +0900

    update src

--- src/util/prefpane/PCKeyboardHackPref.m
+++ src/util/prefpane/PCKeyboardHackPref.m
@@ -17,20 +17,6 @@ static NSString *launchUninstallerCommand = @"/Library/org.pqrs/PCKeyboardHack/e
 }
 
 // ----------------------------------------------------------------------
-- (void) registerLoginWindow
-{
-  NSString *app = @"/Library/org.pqrs/PCKeyboardHack/app/PCKeyboardHack_launchd.app";
-
-  NSString *set_loginwindow = @"/Library/org.pqrs/PCKeyboardHack/bin/set_loginwindow";
-  NSString *result = [BUNDLEPREFIX_Common getExecResult:set_loginwindow args:[NSArray arrayWithObjects:@"exist", app, nil]];
-  if ([result intValue] == 0) {
-    [[NSWorkspace sharedWorkspace] launchApplication:app];
-
-    [BUNDLEPREFIX_Common getExecResult:set_loginwindow args:[NSArray arrayWithObjects:@"set", app, nil]];
-  }
-}
-
-// ----------------------------------------------------------------------
 - (IBAction) launchUninstaller:(id)sender
 {
   [BUNDLEPREFIX_Common getExecResult:launchUninstallerCommand args:[NSArray arrayWithObjects:@"force", nil]];
@@ -40,7 +26,6 @@ static NSString *launchUninstallerCommand = @"/Library/org.pqrs/PCKeyboardHack/e
 - (void) mainViewDidLoad
 {
   [self drawVersion];
-  [self registerLoginWindow];
 }
 
 @end
