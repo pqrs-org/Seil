04ae08686bfaf9518093ed3a62eefd757fc518f6
tekezo <devnull@localhost>
2009-11-28 22:05:59 +0900

    add src/core/server

---
+++ src/core/server/PCKeyboardHack_serverAppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  PCKeyboardHack_serverAppDelegate.h
+//  PCKeyboardHack_server
+//
+//  Created by Takayama Fumihiko on 09/11/28.
+//  Copyright 2009 __MyCompanyName__. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface PCKeyboardHack_serverAppDelegate : NSObject <NSApplicationDelegate> {
+    NSWindow *window;
+}
+
+@property (assign) IBOutlet NSWindow *window;
+
+@end
