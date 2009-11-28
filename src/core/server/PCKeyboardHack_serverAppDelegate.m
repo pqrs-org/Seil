d968edb84fa28f9edfec0cb69917b3f509a2d8c2
Takayama Fumihiko <tekezo@pqrs.org>
2009-11-28 22:05:59 +0900

    add src/core/server

---
+++ src/core/server/PCKeyboardHack_serverAppDelegate.m
@@ -0,0 +1,19 @@
+//
+//  PCKeyboardHack_serverAppDelegate.m
+//  PCKeyboardHack_server
+//
+//  Created by Takayama Fumihiko on 09/11/28.
+//  Copyright 2009 __MyCompanyName__. All rights reserved.
+//
+
+#import "PCKeyboardHack_serverAppDelegate.h"
+
+@implementation PCKeyboardHack_serverAppDelegate
+
+@synthesize window;
+
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification {
+	// Insert code here to initialize your application 
+}
+
+@end
