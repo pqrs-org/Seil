82519befce12e9cefff9df5d0d09e5f40fd8e0c4
Takayama Fumihiko <tekezo@pqrs.org>
2010-01-17 20:47:54 +0900

    add util/uncrustify

---
+++ util/uncrustify/format.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+basedir=`dirname $0`
+
+find ../../* -name '*.cpp' -type f ! -ipath '*/Frameworks/*' ! -ipath '*/kext/*/include/*' | xargs uncrustify -c "$basedir/uncrustify.cfg" --no-backup
+find ../../* -name '*.hpp' -type f ! -ipath '*/Frameworks/*' ! -ipath '*/kext/*/include/*' | xargs uncrustify -c "$basedir/uncrustify.cfg" --no-backup
+find ../../* -name '*.m'   -type f ! -ipath '*/Frameworks/*' ! -ipath '*/kext/*/include/*' | xargs uncrustify -c "$basedir/uncrustify.cfg" --no-backup
+find ../../* -name '*.h'   -type f ! -ipath '*/Frameworks/*' ! -ipath '*/kext/*/include/*' | xargs uncrustify -c "$basedir/uncrustify.cfg" --no-backup -l OC
