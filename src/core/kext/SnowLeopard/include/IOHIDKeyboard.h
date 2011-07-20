a58bc89f0a41dff3b615548915c186f8a3e997e7
Takayama Fumihiko <tekezo@pqrs.org>
2011-07-20 11:39:11 +0900

    update kext/SnowLeopard/include (IOHIDFamily-315.7.16)

--- src/core/kext/SnowLeopard/include/IOHIDKeyboard.h
+++ src/core/kext/SnowLeopard/include/IOHIDKeyboard.h
@@ -31,63 +31,11 @@
 #include "IOHIDConsumer.h"
 #include "IOHIDElement.h"
 #include "IOHIDEventService.h"
+#include "IOHIDFamilyPrivate.h"
 
 enum {
     kUSB_CAPSLOCKLED_SET = 2,
     kUSB_NUMLOCKLED_SET = 1
-};
-
-/* Following table is used to convert Apple USB keyboard IDs into a numbering
-   scheme that can be combined with ADB handler IDs for both Cocoa and Carbon */
-enum {
-    kgestUSBUnknownANSIkd   = 3,       /* Unknown ANSI keyboard */
-    kgestUSBGenericANSIkd   = 40,      /* Generic ANSI keyboard */
-    kgestUSBGenericISOkd    = 41,      /* Generic ANSI keyboard */
-    kgestUSBGenericJISkd    = 42,      /* Generic ANSI keyboard */
-
-    kgestUSBCosmoANSIKbd    = 198,     /* (0xC6) Gestalt Cosmo USB Domestic (ANSI) Keyboard */
-    kprodUSBCosmoANSIKbd    = 0x201,   // The actual USB product ID in hardware
-    kgestUSBCosmoISOKbd     = 199,     /* (0xC7) Cosmo USB International (ISO) Keyboard */
-    kprodUSBCosmoISOKbd     = 0x202,
-    kgestUSBCosmoJISKbd     = 200,     /* (0xC8) Cosmo USB Japanese (JIS) Keyboard */
-    kprodUSBCosmoJISKbd     = 0x203,
-    kgestUSBAndyANSIKbd       = 204,      /* (0xCC) Andy USB Keyboard Domestic (ANSI) Keyboard */
-    kprodUSBAndyANSIKbd     = 0x204,
-    kgestUSBAndyISOKbd      = 205,      /* (0xCD) Andy USB Keyboard International (ISO) Keyboard */
-    kprodUSBAndyISOKbd      = 0x205,
-    kgestUSBAndyJISKbd      = 206,      /* (0xCE) Andy USB Keyboard Japanese (JIS) Keyboard */
-    kprodUSBAndyJISKbd      = 0x206,
-
-    kgestQ6ANSIKbd          = 31,      /* (031) Apple Q6 Keyboard Domestic (ANSI) Keyboard */
-    kprodQ6ANSIKbd          = 0x208,
-    kgestQ6ISOKbd           = 32,      /* (32) Apple Q6 Keyboard International (ISO) Keyboard */
-    kprodQ6ISOKbd           = 0x209,
-    kgestQ6JISKbd           = 33,      /* (33) Apple Q6 Keyboard Japanese (JIS) Keyboard */
-    kprodQ6JISKbd           = 0x20a,
-    
-    kgestQ30ANSIKbd         = 34,      /* (34) Apple Q30 Keyboard Domestic (ANSI) Keyboard */
-    kprodQ30ANSIKbd         = 0x20b,
-    kgestQ30ISOKbd          = 35,      /* (35) Apple Q30 Keyboard International (ISO) Keyboard */
-    kprodQ30ISOKbd          = 0x20c,
-    kgestQ30JISKbd          = 36,      /* (36) Apple Q30 Keyboard Japanese (JIS) Keyboard */
-    kprodQ30JISKbd          = 0x20d,
-    
-    kgestFountainANSIKbd    = 37,      /* (37) Apple Fountain Keyboard Domestic (ANSI) Keyboard */
-    kprodFountainANSIKbd    = 0x20e,
-    kgestFountainISOKbd     = 38,      /* (38) Apple Fountain Keyboard International (ISO) Keyboard */
-    kprodFountainISOKbd     = 0x20f,
-    kgestFountainJISKbd     = 39,      /* (39) Apple Fountain Keyboard Japanese (JIS) Keyboard */
-    kprodFountainJISKbd     = 0x210,
-
-    kgestSantaANSIKbd       = 37,      /* (37) Apple Santa Keyboard Domestic (ANSI) Keyboard */
-    kprodSantaANSIKbd       = 0x211,
-    kgestSantaISOKbd        = 38,      /* (38) Apple Santa Keyboard International (ISO) Keyboard */
-    kprodSantaISOKbd        = 0x212,
-    kgestSantaJISKbd        = 39,      /* (39) Apple Santa Keyboard Japanese (JIS) Keyboard */
-    kprodSantaJISKbd        = 0x213,
-    
-    kgestM89ISOKbd          = 47,      /* (47) Apple M89 Wired (ISO) Keyboard */
-    kgestM90ISOKbd          = 44      /* (44) Apple M90 Wireless (ISO) Keyboard */
 };
 
 #define ADB_CONVERTER_LEN       0xff + 1   //length of array def_usb_2_adb_keymap[]
