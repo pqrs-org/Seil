599b6a3f86d86dd7853a31f012b384eb6e0a9d8b
Takayama Fumihiko <tekezo@pqrs.org>
2011-07-21 02:21:31 +0900

    added src/share

---
+++ src/share/PCKeyboardHackKeys.h
@@ -0,0 +1,11 @@
+// -*- Mode: objc; Coding: utf-8; indent-tabs-mode: nil; -*-
+
+// Keys for userspace applications of PCKeyboardHack.
+
+#define kPCKeyboardHackNotificationKey                @"org.pqrs.PCKeyboardHack.notification"
+#define kPCKeyboardHackServerLaunchedNotification     @"serverLaunched"
+#define kPCKeyboardHackPreferencesChangedNotification @"preferencesChanged"
+
+// ------------------------------------------------------------
+// for NSConnection
+#define kPCKeyboardHackConnectionName                 @"org.pqrs.PCKeyboardHack.server"
