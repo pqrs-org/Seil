7a69dd7f057aae0e9f3ab8045d1759e3fba201e2
Takayama Fumihiko <tekezo@pqrs.org>
2011-07-21 23:40:32 +0900

    update uninstaller

--- src/util/uninstaller/AdminAction.h
+++ src/util/uninstaller/AdminAction.h
@@ -1,11 +1,10 @@
 // -*- Mode: objc; Coding: utf-8; indent-tabs-mode: nil; -*-
 
-#import "bundleprefix.h"
 #import <Cocoa/Cocoa.h>
 #import <Security/Authorization.h>
 #import <Security/AuthorizationTags.h>
 
-@interface BUNDLEPREFIX (AdminAction) : NSObject
+@interface AdminAction : NSObject
 
 + (BOOL) execCommand:(char*)command;
 
