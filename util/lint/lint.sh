66794d2d1c7c0d82dc7b59974193c69f3b78fdf2
Takayama Fumihiko <tekezo@pqrs.org>
2011-07-23 22:38:13 +0900

    update util/lint

--- util/lint/lint.sh
+++ util/lint/lint.sh
@@ -8,6 +8,8 @@ for f in `find $basedir/../../* -name 'project.pbxproj'`; do
     if [ "$(basename $(dirname $(dirname $f)))/$(basename $(dirname $f))" = "prefpane/PCKeyboardHack.xcodeproj" ]; then
         # prefpane needs GC.
         "$basedir/xcodeproj.rb" GCC_ENABLE_OBJC_GC < $f || exit 1
+    elif [ "$(basename $(dirname $(dirname $(dirname $f))))/$(basename $(dirname $(dirname $f)))" = "kext/10.7" ]; then
+        "$basedir/xcodeproj.rb" SDKROOT < $f || exit 1
     else
         "$basedir/xcodeproj.rb" < $f || exit 1
     fi
