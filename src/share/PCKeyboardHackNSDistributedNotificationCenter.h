ef17c1f885c3b57d918404087d7ccb3c28e09379
Takayama Fumihiko <tekezo@pqrs.org>
2011-08-06 00:52:48 +0900

    Added src/share/PCKeyboardHackNSDistributedNotificationCenter

---
+++ src/share/PCKeyboardHackNSDistributedNotificationCenter.h
@@ -0,0 +1,10 @@
+// -*- Mode: objc; Coding: utf-8; indent-tabs-mode: nil; -*-
+
+@interface org_pqrs_PCKeyboardHack_NSDistributedNotificationCenter : NSObject
+
++ (void) addObserver:(id)notificationObserver selector:(SEL)notificationSelector name:(NSString*)notificationName;
++ (void) addObserver:(id)notificationObserver selector:(SEL)notificationSelector name:(NSString*)notificationName object:(NSString*)notificationSender;
++ (void) removeObserver:(id)notificationObserver name:(NSString*)notificationName;
++ (void) postNotificationName:(NSString*)notificationName userInfo:(NSDictionary*)userInfo;
+
+@end
