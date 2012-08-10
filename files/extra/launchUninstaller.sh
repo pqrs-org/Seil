f77bf3d70adce6faa3f0d5879896b14e2a17114b
Takayama Fumihiko <tekezo@pqrs.org>
2012-08-11 00:31:49 +0900

    call setpermissions.sh before launching uninstaller

--- files/extra/launchUninstaller.sh
+++ files/extra/launchUninstaller.sh
@@ -2,4 +2,5 @@
 
 TMPDIR=`/usr/bin/mktemp -d /tmp/PCKeyboardHack_uninstaller.XXXXXX` || exit 1
 /usr/bin/rsync -a /Library/org.pqrs/PCKeyboardHack/app/PCKeyboardHackUninstaller.app "$TMPDIR"
+/Library/org.pqrs/PCKeyboardHack/extra/setpermissions.sh "$TMPDIR/PCKeyboardHackUninstaller.app"
 /usr/bin/open "$TMPDIR/PCKeyboardHackUninstaller.app"
