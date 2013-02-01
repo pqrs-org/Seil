66b88b1b28b85aa724442ffa6d5da9f286b81534
Takayama Fumihiko <tekezo@pqrs.org>
2013-02-01 20:25:58 +0900

    setpermissions to pkginfo

--- make-package.sh
+++ make-package.sh
@@ -76,6 +76,12 @@ done
 #   Please also see postflight.
 #
 sh "files/extra/setpermissions.sh" pkgroot
+sh "files/extra/setpermissions.sh" pkginfo
+chmod 755 \
+    pkginfo/Resources/InstallationCheck \
+    pkginfo/Resources/postflight \
+    pkginfo/Resources/preflight \
+    pkginfo/fixbom.rb
 
 # --------------------------------------------------
 echo "Exec PackageMaker"
