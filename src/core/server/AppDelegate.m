ebea10a6b6c5a493f069f734cef4bb7f2e7a6a7e
Takayama Fumihiko <tekezo@pqrs.org>
2013-11-23 02:52:36 +0900

    Open Preferences if PCKeyboardHack was launched by hand.

--- src/core/server/AppDelegate.m
+++ src/core/server/AppDelegate.m
@@ -107,22 +107,34 @@ static void observer_IONotification(void* refcon, io_iterator_t iterator) {
 }
 
 // ------------------------------------------------------------
+#define kDescendantProcess @"org_pqrs_PCKeyboardHack_DescendantProcess"
+
 - (void) applicationDidFinishLaunching:(NSNotification*)aNotification {
+  NSInteger isDescendantProcess = [[[[NSProcessInfo processInfo] environment] objectForKey:kDescendantProcess] integerValue];
+  setenv([kDescendantProcess UTF8String], "1", 1);
+
+  // ------------------------------------------------------------
+  BOOL fromLaunchAgents = NO;
+
   for (NSString* argument in [[NSProcessInfo processInfo] arguments]) {
     if ([argument isEqualToString:@"--fromLaunchAgents"]) {
-      // ------------------------------------------------------------
-      // Remove old pkg files and finish_installation.app in
-      // "~/Library/Application Support/PCKeyboardHack/.Sparkle".
-      NSArray* paths = NSSearchPathForDirectoriesInDomains(NSApplicationSupportDirectory, NSUserDomainMask, YES);
-      NSString* sparkle = paths[0];
-      if (sparkle) {
-        sparkle = [sparkle stringByAppendingPathComponent:@"PCKeyboardHack"];
-        sparkle = [sparkle stringByAppendingPathComponent:@".Sparkle"];
+      fromLaunchAgents = YES;
+    }
+  }
 
-        NSFileManager* fm = [NSFileManager defaultManager];
-        if ([fm fileExistsAtPath:sparkle]) {
-          [fm removeItemAtPath:sparkle error:nil];
-        }
+  if (fromLaunchAgents) {
+    // ------------------------------------------------------------
+    // Remove old pkg files and finish_installation.app in
+    // "~/Library/Application Support/PCKeyboardHack/.Sparkle".
+    NSArray* paths = NSSearchPathForDirectoriesInDomains(NSApplicationSupportDirectory, NSUserDomainMask, YES);
+    NSString* sparkle = paths[0];
+    if (sparkle) {
+      sparkle = [sparkle stringByAppendingPathComponent:@"PCKeyboardHack"];
+      sparkle = [sparkle stringByAppendingPathComponent:@".Sparkle"];
+
+      NSFileManager* fm = [NSFileManager defaultManager];
+      if ([fm fileExistsAtPath:sparkle]) {
+        [fm removeItemAtPath:sparkle error:nil];
       }
     }
   }
@@ -142,6 +154,13 @@ static void observer_IONotification(void* refcon, io_iterator_t iterator) {
 
   [outlineView_mixed_ initialExpandCollapseTree];
   [updater_ checkForUpdatesInBackground:nil];
+
+  // ------------------------------------------------------------
+  // Open Preferences if KeyRemap4MacBook was launched by hand.
+  if (! fromLaunchAgents &&
+      ! isDescendantProcess) {
+    [preferencesController_ show];
+  }
 }
 
 - (BOOL) applicationShouldHandleReopen:(NSApplication*)theApplication hasVisibleWindows:(BOOL)flag
