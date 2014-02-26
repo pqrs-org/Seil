1f51c4ef78e60eecf926341987f6c42231d16b0a
Takayama Fumihiko <tekezo@pqrs.org>
2014-02-26 13:35:54 +0900

    apply codesign

--- src/core/kext/common/scripts/load.sh
+++ src/core/kext/common/scripts/load.sh
@@ -1,4 +1,13 @@
 #!/bin/sh
 
-sudo cp -R build/Release/PCKeyboardHack.kext /tmp/
-sudo kextutil -t /tmp/PCKeyboardHack.kext
+PATH=/bin:/sbin:/usr/bin:/usr/sbin; export PATH
+
+# --------------------------------------------------
+sudo rm -rf /tmp/PCKeyboardHack
+mkdir /tmp/PCKeyboardHack
+
+sudo cp -R build/Release/PCKeyboardHack.kext /tmp/PCKeyboardHack/PCKeyboardHack.signed.kext
+bash ../../../../files/extra/codesign.sh /tmp/PCKeyboardHack
+sudo chown -R root:wheel /tmp/PCKeyboardHack
+
+sudo kextutil -t /tmp/PCKeyboardHack/PCKeyboardHack.signed.kext
