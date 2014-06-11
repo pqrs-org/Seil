a7fd9d09d3928c2e699809f7ded3c6e27c83969d
Takayama Fumihiko <tekezo@pqrs.org>
2014-06-11 14:10:03 +0900

    remove prefpane check from util/lint

--- util/lint/lint.sh
+++ util/lint/lint.sh
@@ -7,10 +7,7 @@ basedir=`dirname $0`
 for f in `find $basedir/../../* -name 'project.pbxproj'`; do
     echo "Check $f"
 
-    if [ "$(basename $(dirname $(dirname $f)))/$(basename $(dirname $f))" = "prefpane/PCKeyboardHack.xcodeproj" ]; then
-        # prefpane needs GC.
-        "$basedir/xcodeproj.rb" GCC_ENABLE_OBJC_GC < $f || exit 1
-    elif [ "$(basename $(dirname $(dirname $(dirname $f))))/$(basename $(dirname $(dirname $f)))" = "kext/10.8" ]; then
+    if [ "$(basename $(dirname $(dirname $(dirname $f))))/$(basename $(dirname $(dirname $f)))" = "kext/10.8" ]; then
         "$basedir/xcodeproj.rb" SDKROOT < $f || exit 1
     else
         "$basedir/xcodeproj.rb" < $f || exit 1
