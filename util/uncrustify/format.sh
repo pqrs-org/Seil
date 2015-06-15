df32a6c37448237b571a5afaa9be43b785bd3927
Takayama Fumihiko <tekezo@pqrs.org>
2015-06-16 00:31:37 +0900

    remove util/uncrustify

--- util/uncrustify/format.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-basedir=`dirname $0`
-
-find ../../* -name '*.cpp' -type f ! -ipath '*/Pods/*' ! -ipath '*/build/*' ! -ipath '*/kext/*/*include/*' ! -ipath '*/src/bridge/output/*' | xargs uncrustify -c "$basedir/uncrustify.cfg" --no-backup
-find ../../* -name '*.hpp' -type f ! -ipath '*/Pods/*' ! -ipath '*/build/*' ! -ipath '*/kext/*/*include/*' ! -ipath '*/src/bridge/output/*' | xargs uncrustify -c "$basedir/uncrustify.cfg" --no-backup
-find ../../* -name '*.m'   -type f ! -ipath '*/Pods/*' ! -ipath '*/build/*' ! -ipath '*/kext/*/*include/*' ! -ipath '*/src/bridge/output/*' | xargs uncrustify -c "$basedir/uncrustify.cfg" --no-backup
-find ../../* -name '*.h'   -type f ! -ipath '*/Pods/*' ! -ipath '*/build/*' ! -ipath '*/kext/*/*include/*' ! -ipath '*/src/bridge/output/*' | xargs uncrustify -c "$basedir/uncrustify.cfg" --no-backup -l OC
