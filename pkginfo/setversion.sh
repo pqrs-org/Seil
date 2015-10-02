35300b2549aea7f4f714ad93c2cabb2a55a27376
Takayama Fumihiko <tekezo@pqrs.org>
2015-10-03 02:33:51 +0900

    support directory includes white space

--- pkginfo/setversion.sh
+++ pkginfo/setversion.sh
@@ -3,8 +3,17 @@
 basedir=`dirname $0`
 version=$(cat "$basedir/../version")
 
-for f in `find $basedir/.. -name 'Info.plist.tmpl' -or -name 'Distribution.xml.tmpl'`; do
+find $basedir/.. \( -name 'Info.plist.tmpl' -or -name 'Distribution.xml.tmpl' \) -print0 | while IFS= read -r -d '' f; do
     if [ -f "$f" ]; then
-        sed "s|PKGVERSION|$version|g" $f > `dirname $f`/`basename $f .tmpl`
+        outputfile=`dirname "$f"`/`basename "$f" .tmpl`
+        tmpfile=`mktemp /tmp/Info.plist.XXXXXX`
+
+        sed "s|PKGVERSION|$version|g" "$f" > $tmpfile
+        if cmp -s "$tmpfile" "$outputfile"; then
+            # tmpfile and outputfile are same. remove tmpfile.
+            rm -f "$tmpfile"
+        else
+            mv "$tmpfile" "$outputfile"
+        fi
     fi
 done
