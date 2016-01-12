6c6bb6ea4a4ccdeac30e39312ffd2a1a4b43c4dd
Takayama Fumihiko <tekezo@pqrs.org>
2016-01-12 14:23:14 +0900

    fix PATH

--- make-package.sh
+++ make-package.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 # set $GEM_HOME/bin/ for CocoaPods.
-PATH="/bin:/sbin:/usr/bin:/usr/sbin:$GEM_HOME/bin"; export PATH
+PATH="/bin:/usr/bin:/usr/local/bin:$GEM_HOME/bin"; export PATH
 
 version=$(cat version)
 
