20f5eb2c60263a3d7b7a7503d1222b5c3c6fa81a
Takayama Fumihiko <tekezo@pqrs.org>
2016-03-16 00:39:25 +0900

    add kPreferencesChangedNotificationUserInfoKeyPreferencesChangedFromGUI

--- src/core/server/Classes/NotificationKeys.h
+++ src/core/server/Classes/NotificationKeys.h
@@ -1,3 +1,5 @@
 // -*- Mode: objc; Coding: utf-8; indent-tabs-mode: nil; -*-
 
 #define kPreferencesChangedNotification @"PreferencesChanged"
+
+#define kPreferencesChangedNotificationUserInfoKeyPreferencesChangedFromGUI @"kPreferencesChangedNotificationUserInfoKeyPreferencesChangedFromGUI"
