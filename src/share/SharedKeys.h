73748bcb6bb297f793c43ce80ea7a8118f7aee68
Takayama Fumihiko <tekezo@pqrs.org>
2016-04-11 00:31:37 +0900

    add kSeilPreferencesUpdatedNotification

--- src/share/SharedKeys.h
+++ src/share/SharedKeys.h
@@ -1,5 +1,9 @@
 // -*- Mode: objc; Coding: utf-8; indent-tabs-mode: nil; -*-
 
 // ------------------------------------------------------------
+// NSDistributedNotificationCenter
+#define kSeilPreferencesUpdatedNotification @"org.pqrs.Seil.notification.PreferencesUpdated"
+
+// ------------------------------------------------------------
 // NSConnection
 #define kSeilConnectionName @"org.pqrs.Seil.server"
